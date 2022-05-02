41ccef90ce89dc35807be9bef5dabf7e2ef2ccf7
Cristian Dobre <cristian.dobre98@yahoo.com>
2022-05-02 14:12:50 +0300

    functii goale

---
+++ battleship.py
@@ -0,0 +1,64 @@
+
+# returneaza player_versus
+def get_game_mde():
+    pass 
+
+# returneaza board_size
+def get_board_size():
+    pass
+
+# returneaza ships_number
+def get_ships_number():
+    pass
+
+# returneaza turns number
+def get_turns_to_play():
+    pass
+
+# returneaza board
+def generate_board(board_size):
+    pass
+
+# printeazaz boardul
+def print_board(board, board_size):
+    pass
+
+# returneaza ships
+def generate_ships(ships_number):
+    pass
+
+# returneaza player_coordinates
+def get_coordinates(board_size):
+    pass
+
+# returneaza ship_direction, valid_direction
+def validate_ship_position(row,column, board):
+    pass
+
+# return board
+def place_ships(ships, board, board_size):
+    pass
+
+# return board
+def spacing_ships(board):
+    pass
+
+# return hidden board
+def update_board_after_shoot(player_board, hidden_board, board_size):
+    pass
+
+# returneaza true sau false
+def win_condition(player_one_board, player_two_board):
+    pass
+
+# returneaza true sau false
+def tie_condition(turns, counter):
+    pass
+
+
+def play_with_AI():
+    pass
+
+
+def main():
+    pass
