c375a0c73338529cf3187806f21956cf7f2aabe7
Toma <bucurtomaadrian@gmail.com>
2022-05-03 12:57:15 +0300

    ggg

--- battleship.py
+++ battleship.py
@@ -51,7 +51,50 @@ def validate_ship_position(row,column, board):
 
 # return board
 def place_ships(ships, board, board_size):
-    pass
+
+    for ship in ships:
+        while True:
+            print_board(board,board_size)
+            ship_coordinates = get_coordinates(board_size)
+            row = ord(ship_coordinates[0]) - ord("a")
+            column = int(ship_coordinates[1])-1
+            ship_direction,valid_directions = validate_ship_position(row,column,board)
+            if ship_direction != "\nNow, please select the direction you want the ship to go":
+                while True:
+                    user_choice = input(ship_direction)
+                    if user_choice in valid_directions:
+                        if user_choice == "1":
+                            board[row][column] = "T" 
+                            board[row+1][column] = "T"
+                            board[row+2][column] = "T"
+                            break
+                        elif user_choice == "2":
+                            board[row][column] = "T" 
+                            board[row][column+1] = "T"
+                            board[row][column+2] = "T"
+                            break
+                        elif user_choice == "3":
+                            board[row][column] = "T" 
+                            board[row-1][column] = "T"
+                            board[row-2][column] = "T"
+                            break
+                        elif user_choice == "4":
+                            board[row][column] = "T" 
+                            board[row][column-1] = "T"
+                            board[row][column-2] = "T"
+                            break
+
+                    else:
+                        print("Please make a valid input")
+
+                spacing_ships(board)
+                break
+
+            else:
+                print("\nPlease try again, looks like there wasn't enough space for the ship to be placed.")
+
+    
+    return board
 
 #ships not closer than 1 space
 def spacing_ships(board):
