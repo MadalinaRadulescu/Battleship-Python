8a3dae0b94ab9679e95da587b20ae5a4cb6e629b
Toma <bucurtomaadrian@gmail.com>
2022-05-03 09:58:22 +0300

    spacing ships

--- battleship.py
+++ battleship.py
@@ -1,6 +1,6 @@
 
 # returneaza player_versus
-def get_game_mde():
+def get_game_mode():
     pass 
 
 # returneaza board_size
@@ -35,13 +35,55 @@ def get_coordinates(board_size):
 def validate_ship_position(row,column, board):
     pass
 
+#####
+#####
+# DE AICI IN SUS
+# MIHAI
+# RAUL
+# CRISTI
+#####
+#####
+# DE AICI IN JOS 
+# MADA
+# TOMA
+#####
+#####
+
 # return board
 def place_ships(ships, board, board_size):
     pass
 
-# return board
+#ships not closer than 1 space
 def spacing_ships(board):
-    pass
+
+    m=0
+    for j in board:
+        n=0
+        for i in j:
+            
+            if i == "T":
+                try:
+                    if n+1 < len(board[0]) and j[n+1] == "-":
+                        j[n+1] = "Z"
+                except IndexError:
+                    pass
+                try:
+                    if n-1 >= 0 and j[n-1] == "-":
+                        j[n-1] = "Z"
+                except IndexError:
+                    pass
+                try:
+                    if m+1 < len(board) and board[m+1][n] == "-":
+                        board[m+1][n] = "Z"
+                except IndexError:
+                    pass
+                try:
+                    if m-1 >= 0 and board[m-1][n] == "-":
+                        board[m-1][n] = "Z"
+                except IndexError:
+                    pass
+            n+=1   
+        m+=1        
 
 # return hidden board
 def update_board_after_shoot(player_board, hidden_board, board_size):
@@ -55,10 +97,16 @@ def win_condition(player_one_board, player_two_board):
 def tie_condition(turns, counter):
     pass
 
-
+# Optional
 def play_with_AI():
     pass
 
 
 def main():
     pass
+
+
+
+
+if __name__ == "__main__":
+    main()
