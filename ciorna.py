d395dd753bb7f1db23bb50c5e1db6e867a611dac
Toma <bucurtomaadrian@gmail.com>
2022-05-04 22:33:56 +0300

    Bugs fixed, AI(dumb)-No double-boards/timesleep/all parts H->no S

--- ciorna.py
+++ ciorna.py
@@ -1,19 +1,25 @@
 
 
-board = [["-", "-", "-","-", "-"],["-", "-", "-","-", "-"],["-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-"]]
-def print_board(board, board_size):
+# board = [["-", "-", "-","-", "-"],["-", "-", "-","-", "-"],["-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-"], ["-", "-", "-", "-", "-"]]
+# def print_board(board, board_size):
 
-    letters = " ABCDEFGHIJ"
-    first_row = "  "
-    n = 0
-    for i in range(1,board_size+1):
-        first_row+= " "
-        first_row+=(" ".join(str(i)))
+#     letters = " ABCDEFGHIJ"
+#     first_row = "  "
+#     n = 0
+#     for i in range(1,board_size+1):
+#         first_row+= " "
+#         first_row+=(" ".join(str(i)))
         
-    print(f"\n{first_row}")
-    for i in board:
-        n += 1
-        print(str(letters[n])+ "  " + " ".join(i))
+#     print(f"\n{first_row}")
+#     for i in board:
+#         n += 1
+#         print(str(letters[n])+ "  " + " ".join(i))
 
 
-print_board(board, 5)
+# print_board(board, 5)
+
+
+ship = ["T","T"]
+
+print(len(ship))
+
